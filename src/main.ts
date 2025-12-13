015a5320e7aba583a5c724ee66352636188b96d0
jeffpham <jeffpham2906@gmail.com>
2025-12-13 22:32:50 +0700

    feat: add Swagger integration for API documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Media Service API')
+    .setDescription('API documentation for the Media Service')
+    .setVersion(version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
